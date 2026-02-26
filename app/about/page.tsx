c26cca676ed622a55513fe1aa54e73e57fe5431b
Touko Seppä <touko2002seppa@gmail.com>
2026-02-26 19:40:53 +0200

    Modified plain text

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,8 +1,13 @@
 export default function About() {
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-10">
-            <h1 className="text-4xl font-bold">About Us</h1>
-            <p className="mt-4 text-lg text-gray-600">©Tsseppa</p>
+        <main className="flex min-h-screen flex-col items-center p-10">
+            <h1 className="text-4xl font-bold">About Me</h1>
+            <div className="mt-6 align-center text-center">
+                <p className="mt-4 text-lg text-gray-600">
+                    This blog is for me to express my thoughts. I hope you find it relatable. This is also for me to train my skills as computer scientist. 
+                </p>
+            </div>
+            
         </main>
     );
 }
