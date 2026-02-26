b4797f25842151606385aa5b57a9cf4d93a87212
Touko Seppä <touko.seppa@aalto.fi>
2026-02-26 15:27:08 +0200

    Created blog page

---
+++ app/blog/page.tsx
@@ -0,0 +1,7 @@
+export default function Blog() {
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-10">
+            <h1 className="text-4xl font-bold">Blog</h1>
+        </main>
+    )
+}
