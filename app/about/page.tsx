e21f23bfdaf961fb902d0160a8a5c7472767c14d
Touko Seppä <touko.seppa@aalto.fi>
2026-02-26 15:26:46 +0200

    created about page

---
+++ app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function About() {
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-10">
+            <h1 className="text-4xl font-bold">About Us</h1>
+            <p className="mt-4 text-lg text-gray-600">©Tsseppa</p>
+        </main>
+    );
+}
