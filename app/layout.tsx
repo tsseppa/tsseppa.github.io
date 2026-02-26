96f1486e599c872fa4296347192f1a3a3b3e8cda
Touko Seppä <touko2002seppa@gmail.com>
2026-02-26 19:41:16 +0200

    Added Footer to Layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 import Navbar from "@/components/Navbar"
+import Footer from "@/components/Footer"
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
@@ -9,6 +10,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body>
         <Navbar />
         <main>{children}</main>
+        <Footer />
       </body>
     </html>
   )
